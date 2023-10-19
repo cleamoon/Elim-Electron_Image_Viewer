cb9e6ad7f727d3d337fc4e3981a157165726358b
Yue Jiao <cleamoon@hotmail.com>
2023-10-19 11:12:17 +0200

    Move file management to separated file

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,32 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-
-const filePath = ref<string>('')
-
-const folderPath = computed(() => filePath.value?.split('/').slice(0, -1).join('/'))
-
-const filesInFolder = ref<string[]>([])
-
-const fileIndexInFolder = computed(() =>
-  filePath.value && filesInFolder.value.length && filesInFolder.value.indexOf(filePath.value))
-
-const file = computed(() => {
-  if (filesInFolder.value && fileIndexInFolder.value) {
-    return filesInFolder.value[fileIndexInFolder.value]
-  }
-  return ''
-})
-
-const image = ref<string>('')
-
-const parentFolder = computed(() => {
-  if (folderPath.value) {
-    return folderPath.value.split('/').slice(0, -1).join('/')
-  }
-  return ''
-})
-
-const foldersInParentFolder = ref<string[]>([])
+import { image, filePath, folderPath, filesInFolder, parentFolder, foldersInParentFolder, file } from './components/FileManager.vue';
 
 const openImage = () => {
   window.fileManaging.openFile()
@@ -38,9 +11,6 @@ const openImage = () => {
     .then(() => window.fileManaging.readImage(file.value))
     .then((imageBlob) => image.value = imageBlob)
 }
-
-
-window.electronAPI.sendMessage('Hello from App.vue!');
 </script>
 
 <template>
---
+++ src/renderer/components/FileManager.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { ref, computed } from 'vue'
+
+export const filePath = ref<string>('')
+
+export const folderPath = computed(() => filePath.value?.split('/').slice(0, -1).join('/'))
+
+export const filesInFolder = ref<string[]>([])
+
+export const fileIndexInFolder = computed(() =>
+  filePath.value && filesInFolder.value.length && filesInFolder.value.indexOf(filePath.value))
+
+export const file = computed(() =>
+  filesInFolder.value.length && fileIndexInFolder.value >= 0 && filesInFolder.value[fileIndexInFolder.value])
+
+export const image = ref<string>('')
+
+export const parentFolder = computed(() => folderPath.value?.split('/').slice(0, -1).join('/'))
+
+export const foldersInParentFolder = ref<string[]>([])
+</script>
