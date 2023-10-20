90accc1d706b966053c661970b881de447b6293d
Yue Jiao <cleamoon@hotmail.com>
2023-10-20 15:08:47 +0200

    Change file manager logic

--- src/renderer/components/FileManager.vue
+++ src/renderer/components/FileManager.vue
@@ -3,19 +3,34 @@ import { ref, computed } from 'vue'
 
 export const filePath = ref<string>('')
 
-export const folderPath = computed(() => filePath.value?.split('/').slice(0, -1).join('/'))
+export const folderPath = ref<string>('')
+
+export const parentFolderPath = ref<string>('')
 
 export const filesInFolder = ref<string[]>([])
 
-export const fileIndexInFolder = computed(() =>
-  filePath.value && filesInFolder.value.length && filesInFolder.value.indexOf(filePath.value))
+export const foldersInParentFolder = ref<string[]>([])
+
+export const fileIndex = ref<number>(-1)
+
+export const folderIndex = ref<number>(-1)
 
 export const file = computed(() =>
-  filesInFolder.value.length && fileIndexInFolder.value >= 0 && filesInFolder.value[fileIndexInFolder.value])
+  filesInFolder.value.length && fileIndex.value >= 0 && filesInFolder.value[fileIndex.value])
 
 export const image = ref<string>('')
 
-export const parentFolder = computed(() => folderPath.value?.split('/').slice(0, -1).join('/'))
-
-export const foldersInParentFolder = ref<string[]>([])
+export const changeFolder = (direction: number) => {
+  if (direction > 0 && folderIndex.value + 1 <= foldersInParentFolder.value.length) {
+      folderIndex.value += 1;
+      window.fileManaging.getFilesInFolder(folderPath.value)
+        .then((files) => filesInFolder.value = files.map((file) => folderPath.value + "/" + file))
+        .then(() => fileIndex.value = 0)
+  } else if (direction < 0 && folderIndex.value - 1 >= 0) {
+      folderIndex.value -= 1;
+      window.fileManaging.getFilesInFolder(folderPath.value)
+        .then((files) => filesInFolder.value = files.map((file) => folderPath.value + "/" + file))
+        .then((files) => fileIndex.value = files.length - 1)
+  }
+}
 </script>
