f385843fc555d94b38bcde4d8965c9195157efc6
Yue Jiao <cleamoon@hotmail.com>
2023-10-21 17:35:33 +0200

    Rewrite logic for folderPath

--- src/renderer/components/FileManager.vue
+++ src/renderer/components/FileManager.vue
@@ -3,8 +3,6 @@ import { ref, computed } from 'vue'
 
 export const filePath = ref<string>('')
 
-export const folderPath = ref<string>('')
-
 export const parentFolderPath = ref<string>('')
 
 export const filesInFolder = ref<string[]>([])
@@ -15,20 +13,43 @@ export const fileIndex = ref<number>(-1)
 
 export const folderIndex = ref<number>(-1)
 
+export const folderPath = computed(() =>
+  foldersInParentFolder.value.length && folderIndex.value >= 0 && foldersInParentFolder.value[folderIndex.value])
+
 export const file = computed(() =>
   filesInFolder.value.length && fileIndex.value >= 0 && filesInFolder.value[fileIndex.value])
 
 export const changeFolder = (direction: number) => {
   if (direction > 0 && folderIndex.value + 1 <= foldersInParentFolder.value.length) {
-      folderIndex.value += 1;
-      window.fileManaging.getFilesInFolder(folderPath.value)
-        .then((files) => filesInFolder.value = files.map((file) => folderPath.value + "/" + file))
-        .then(() => fileIndex.value = 0)
+    folderIndex.value += 1;
+    window.fileManaging.getFilesInFolder(folderPath.value || '')
+      .then((files) => filesInFolder.value = files.map((file) => folderPath.value + "/" + file))
+      .then(() => fileIndex.value = 0)
   } else if (direction < 0 && folderIndex.value - 1 >= 0) {
-      folderIndex.value -= 1;
-      window.fileManaging.getFilesInFolder(folderPath.value)
-        .then((files) => filesInFolder.value = files.map((file) => folderPath.value + "/" + file))
-        .then((files) => fileIndex.value = files.length - 1)
+    folderIndex.value -= 1;
+    window.fileManaging.getFilesInFolder(folderPath.value || '')
+      .then((files) => filesInFolder.value = files.map((file) => folderPath.value + "/" + file))
+      .then((files) => fileIndex.value = files.length - 1)
   }
 }
+
+export const openImage = () => {
+  const fileManaging = window.fileManaging;
+  fileManaging.openFile()
+    .then((filePath) => {
+      const folder = filePath.split('/').slice(0, -1).join('/');
+      const parentFolder = filePath.split('/').slice(0, -2).join('/');
+      return ({ folder, parentFolder })
+    })
+    .then(({ folder, parentFolder }) => {
+      parentFolderPath.value = parentFolder;
+
+      fileManaging.getFilesInFolder(folder)
+        .then((files) => filesInFolder.value = files.map((file) => folderPath.value + "/" + file))
+        .then((files) => fileIndex.value = files.indexOf(filePath.value))
+      fileManaging.getFoldersInFolder(parentFolder)
+        .then((folders) => foldersInParentFolder.value = folders)
+        .then((folders) => folderIndex.value = folders.indexOf(folder))
+    })
+};
 </script>
