7a8f7d5274aa101a3cdd93b43e857d404670b759
Yue Jiao <cleamoon@hotmail.com>
2023-10-22 17:20:06 +0200

    Improve image view

--- src/renderer/components/Image.vue
+++ src/renderer/components/Image.vue
@@ -2,6 +2,7 @@
 import { watchEffect } from "vue";
 import { image, imageStyle, showFilePath } from "./ImageStates.vue";
 import { file, openImage } from "./FileManager.vue";
+import Help from "./Help.vue";
 
 watchEffect(() => {
   file.value && window.fileManaging.readImage(file.value)
@@ -11,11 +12,16 @@ watchEffect(() => {
 
 <template>
   <div id="main">
-    <img :src="image" v-if="image" :style="imageStyle" />
-    <div v-if="showFilePath" id="info">
-      {{ file }}
+    <div v-if="Boolean(image)" :style="{ width: '100%', height: '100%' }">
+      <img :src="image" v-if="image" :style="imageStyle" />
+      <div v-if="showFilePath" id="info">
+        {{ file }}
+      </div>
     </div>
-    <button @click="openImage" v-if="!Boolean(image)">Open Image</button>
+    <div v-if="!Boolean(image)" :style="{ width: '100%', height: '100%' }">
+      <Help />
+      <button @click="openImage" id="button">Open Image</button>
+    </div>
   </div>
 </template>
 
@@ -28,6 +34,7 @@ watchEffect(() => {
   justify-content: center;
   width: 100vw;
   height: 100vh;
+  background-color: lavender;
 }
 
 #info {
@@ -39,5 +46,13 @@ watchEffect(() => {
   font-size: 0.6rem;
   color: rgba(50, 255, 50, 1);
   background-color: rgba(10, 10, 10, 0.6);
+}
+
+#button {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: auto;
+  bottom: auto;
 }
 </style>
