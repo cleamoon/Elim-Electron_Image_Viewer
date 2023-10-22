ccc124ec41bfe6c6c0068ccd4d66f2eb5e1b94db
Yue Jiao <cleamoon@hotmail.com>
2023-10-22 13:20:33 +0200

    Correct file manager logic and add sorter

--- src/renderer/components/FileManager.vue
+++ src/renderer/components/FileManager.vue
@@ -1,8 +1,6 @@
 <script lang="ts">
 import { ref, computed } from 'vue'
 
-export const filePath = ref<string>('')
-
 export const parentFolderPath = ref<string>('')
 
 export const filesInFolder = ref<string[]>([])
@@ -16,19 +14,28 @@ export const folderIndex = ref<number>(-1)
 export const folderPath = computed(() =>
   foldersInParentFolder.value.length && folderIndex.value >= 0 && foldersInParentFolder.value[folderIndex.value])
 
-export const file = computed(() =>
-  filesInFolder.value.length && fileIndex.value >= 0 && filesInFolder.value[fileIndex.value])
+export const file = computed(() => filesInFolder.value[fileIndex.value])
+
+const sorter = (a: string, b: string) => {
+  const numberInA = a.match(/\d+/g);
+  const numberInB = b.match(/\d+/g);
+  if (numberInA && numberInB) {
+    return parseInt(numberInA[0]) - parseInt(numberInB[0]);
+  }
+  return a.localeCompare(b);
+}
 
 export const changeFolder = (direction: number) => {
   if (direction > 0 && folderIndex.value + 1 <= foldersInParentFolder.value.length) {
     folderIndex.value += 1;
+    console.log(folderPath.value)
     window.fileManaging.getFilesInFolder(folderPath.value || '')
-      .then((files) => filesInFolder.value = files.map((file) => folderPath.value + "/" + file))
+      .then((files) => filesInFolder.value = files.sort(sorter).map((file) => folderPath.value + "/" + file))
       .then(() => fileIndex.value = 0)
   } else if (direction < 0 && folderIndex.value - 1 >= 0) {
     folderIndex.value -= 1;
     window.fileManaging.getFilesInFolder(folderPath.value || '')
-      .then((files) => filesInFolder.value = files.map((file) => folderPath.value + "/" + file))
+      .then((files) => filesInFolder.value = files.sort(sorter).map((file) => folderPath.value + "/" + file))
       .then((files) => fileIndex.value = files.length - 1)
   }
 }
@@ -37,18 +44,21 @@ export const openImage = () => {
   const fileManaging = window.fileManaging;
   fileManaging.openFile()
     .then((filePath) => {
+      if (!filePath) throw new Error('No file selected');
+
       const folder = filePath.split('/').slice(0, -1).join('/');
       const parentFolder = filePath.split('/').slice(0, -2).join('/');
-      return ({ folder, parentFolder })
+      return ({ filePath, folder, parentFolder })
     })
-    .then(({ folder, parentFolder }) => {
+    .then(({ filePath, folder, parentFolder }) => {
       parentFolderPath.value = parentFolder;
 
       fileManaging.getFilesInFolder(folder)
-        .then((files) => filesInFolder.value = files.map((file) => folderPath.value + "/" + file))
-        .then((files) => fileIndex.value = files.indexOf(filePath.value))
+        .then((files) => filesInFolder.value = files.sort(sorter).map((file) => folder + "/" + file))
+        .then((files) => fileIndex.value = files.indexOf(filePath))
+
       fileManaging.getFoldersInFolder(parentFolder)
-        .then((folders) => foldersInParentFolder.value = folders)
+        .then((folders) => foldersInParentFolder.value = folders.sort(sorter).map((folder) => parentFolder + "/" + folder))
         .then((folders) => folderIndex.value = folders.indexOf(folder))
     })
 };
